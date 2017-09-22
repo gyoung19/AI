9c3497b0cab8f70e713661f3a60d875fc0001236
gyoung19 <31937633+gyoung19@users.noreply.github.com>
2017-09-22 17:26:56 -0400

    Q8 completed
    Gets full marks from the autograder but the description of the question asks that the program use an appropriate search method for finding the closest dot. It's easy enough to play around with the different search methods, just change the return statement at the end of mazePath which is currently set to A*.

--- searchAgents.py
+++ searchAgents.py
@@ -411,7 +411,6 @@ def cornersHeuristic(state, problem):
     STRATEGY:
     h(n) = optimistic estimate of the distance to the nearest food + 
             the minimum distance from that food to the farthest food left
-
     This heuristic is more optimistic than cornersHeuristic_totalTraversal.
     Yet I am not sure if h_totalTraversal is really consistent.
     """
@@ -601,6 +600,8 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
+        nearestFood, shortestDistance = findNearestFood_greedy(startPosition, food, problem)
+        return mazePath(startPosition, nearestFood, gameState)
         util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -634,6 +635,18 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
+        
+        isGoal = state == self.goal
+
+        # For display purposes only
+        if isGoal and self.visualize:
+            self._visitedlist.append(state)
+            import __main__
+            if '_display' in dir(__main__):
+                if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
+                    __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
+
+        return isGoal
         util.raiseNotDefined()
 
 def mazeDistance(point1, point2, gameState):
@@ -651,3 +664,11 @@ def mazeDistance(point1, point2, gameState):
     assert not walls[x2][y2], 'point2 is a wall: ' + str(point2)
     prob = PositionSearchProblem(gameState, start=point1, goal=point2, warn=False, visualize=False)
     return len(search.bfs(prob))
+def mazePath(point1, point2, gameState):
+    x1, y1 = point1
+    x2, y2 = point2
+    walls = gameState.getWalls()
+    assert not walls[x1][y1], 'point1 is a wall: ' + str(point1)
+    assert not walls[x2][y2], 'point2 is a wall: ' + str(point2)
+    prob = PositionSearchProblem(gameState, start=point1, goal=point2, warn=False, visualize=False)
+    return search.aStarSearch(prob)
