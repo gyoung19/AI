fabeb7c2285affa28f554ed711f54d5adcba2ab1
gyoung19 <31937633+gyoung19@users.noreply.github.com>
2017-10-16 21:39:17 -0400

    Add files via upload

--- multiAgents.py
+++ multiAgents.py
@@ -15,6 +15,8 @@
 from util import manhattanDistance
 from game import Directions
 import random, util
+#added code 
+import sys
 
 from game import Agent
 
@@ -206,9 +208,6 @@ def minimax(self, gameState, depth, agentIndex):
         ghostValues = [minimax(self,child,depth,agentIndex+1) for child in ghostChildren]
         Min = min(ghostValues)
         return Min
-            
-            
-       
         
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -220,7 +219,59 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
           Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+        action = alphabeta(self,gameState,self.depth,0,-sys.maxsize-1,sys.maxsize)
+        return action
         util.raiseNotDefined()
+
+def alphabeta(self,gameState,depth,agentIndex,alpha,beta):
+#pacman's turn
+    numAgents = gameState.getNumAgents()
+    #resets to pacman's turn after a number of ghost turns
+    if agentIndex%numAgents is 0:
+        agentIndex = 0
+
+    if agentIndex is 0:
+        pacActions = gameState.getLegalActions(0)
+        #returns evaluation at depth limit or leaves
+        if len(pacActions) is 0:
+            return self.evaluationFunction(gameState)
+        if depth is 0:
+            return self.evaluationFunction(gameState)
+        else:
+            Max = -sys.maxsize-1
+            t = 0
+            for action in pacActions:
+                child = gameState.generateSuccessor(0,action)
+                tmp = max(Max,alphabeta(self,child,depth,1,alpha,beta))
+                if tmp is not Max:
+                    Max = tmp
+                    t = pacActions.index(action)
+                alpha = max(alpha,Max)
+                if beta < alpha:
+                    break;
+        #returns an action from initial node, value from anywhere else
+        if depth is self.depth:
+            return pacActions[t]
+        else:
+            return Max
+
+    #ghosts' turns
+    else:
+        ghostActions = gameState.getLegalActions(agentIndex)
+        #return for leaves
+        if len(ghostActions) is 0:
+            return self.evaluationFunction(gameState)
+        #checks if next turn should be pacman's and decrements depth accordingly
+        if agentIndex is numAgents-1:
+            depth = depth-1
+        Min = sys.maxsize
+        for action in ghostActions:
+            child = gameState.generateSuccessor(agentIndex,action)
+            Min = min(Min,alphabeta(self,child,depth,agentIndex+1,alpha,beta))
+            beta = min(Min,beta)
+            if alpha > beta:
+                break
+        return Min
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -235,7 +286,46 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
           legal moves.
         """
         "*** YOUR CODE HERE ***"
+        return expectimax(self,gameState,self.depth,0)
         util.raiseNotDefined()
+
+def expectimax(self, gameState, depth, agentIndex):
+    #pacman's turn
+    numAgents = gameState.getNumAgents()
+    #resets to pacman's turn after a number of ghost turns
+    if agentIndex%numAgents is 0:
+        agentIndex = 0
+
+    if agentIndex is 0:
+        pacActions = gameState.getLegalActions(0)
+        #returns evaluation at depth limit or leaves
+        if len(pacActions) is 0:
+            return self.evaluationFunction(gameState)
+        if depth is 0:
+            return self.evaluationFunction(gameState)
+        else:
+            children = [gameState.generateSuccessor(0,action) for action in pacActions]    
+            values = [expectimax(self,child,depth,1) for child in children]
+        Max = max(values)
+        #returns an action from initial node, value from anywhere else
+        if depth is self.depth:
+            return pacActions[values.index(Max)]
+        else:
+            return Max
+
+    #ghosts' turns
+    else:
+        ghostActions = gameState.getLegalActions(agentIndex)
+        #return for leaves
+        if len(ghostActions) is 0:
+            return self.evaluationFunction(gameState)
+        ghostChildren = [gameState.generateSuccessor(agentIndex,action) for action in ghostActions]
+        #checks if next turn should be pacman's and decrements depth accordingly
+        if agentIndex is numAgents-1:
+            depth = depth-1
+        ghostValues = [expectimax(self,child,depth,agentIndex+1) for child in ghostChildren]
+        Avg = sum(ghostValues)/len(ghostValues)
+        return Avg
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -245,6 +335,45 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
+    #successorGameState = currentGameState.generatePacmanSuccessor(action)
+    pacPos = currentGameState.getPacmanPosition()
+    feed = currentGameState.getFood()
+    ghostStates = currentGameState.getGhostStates()
+    scaredTimes = [ghostState.scaredTimer for ghostState in ghostStates]
+
+    bonus = 0
+    ghostPositions = [ghostState.getPosition() for ghostState in ghostStates]
+    closestGhostDistance = currentGameState.getWalls().height + currentGameState.getWalls().width # MAX
+    #print newGhostPositions
+    for position in ghostPositions:
+        givenGhostDistance=manhattanDistance(pacPos,position)
+        if givenGhostDistance < closestGhostDistance:
+            closestGhostDistance = givenGhostDistance
+        #deincentivize only if ghost is within two moves
+    if closestGhostDistance < 3:
+            bonus -= 100
+    shortestDistance = currentGameState.getWalls().height + currentGameState.getWalls().width # MAX
+    farthestDistance = 0 #MIN
+    nearestFood = None
+    farthestFood = None
+    #for loop finds the nearest distance to food
+    for food in feed.asList():
+        #print food
+            givenDistance = manhattanDistance(pacPos, food)
+        #print givenDistance
+            if givenDistance < shortestDistance:
+                shortestDistance = givenDistance
+                nearestFood = food
+            if givenDistance > farthestDistance:
+                farthestDistance = givenDistance
+                farthestFood = food
+    #should incentivize eating food somehow
+    
+        #print successorGameState.getScore()-shortestDistance + bonus        
+
+        #Pacman is currently incentivized to go for the nearest food and eat it unless it is close to a ghost
+    return currentGameState.getScore()
+
     util.raiseNotDefined()
 
 # Abbreviation
